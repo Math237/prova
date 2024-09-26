c91ac1a64afc65bbc28b6439fccdee2ab77a50bd
Math237 <richardgarcia28688@gmail.com>
2024-09-25 22:25:43 -0300

    prova

--- ex4/ex4.js
+++ ex4/ex4.js
@@ -20,13 +20,17 @@ Crédito - sem desconto.
 Apresente todos os dados da compras, com seus calculos e valores;
 */
 
-var menuLanches = prompt("==== Confira o cardápio abaixo ====");
-menuLanches = menuLanches + prompt("Código | lanche | Preço\n");
-menuLanches += prompt("1 -> X-SALADA-> R$ 20,00\n");
-menuLanches += prompt("2 -> X-BACON-> R$ 25,00\n");
-menuLanches += prompt("3 -> X-TUDO-> R$ 23,00\n");
-menuLanches += prompt("4 -> BAURU -> R$ 29,99\n");
-menuLanches += prompt("5 -> AMERICANO -> 22,50\n");
-menuLanches += prompt("Digite o código do lanche que deseja pedir:");
-console.log = parseInt(prompt(menuLanches));
- 
+var menuLanches = "==== Confira o cardápio abaixo ==== \n";
+menuLanches = menuLanches + "Código | lanche | Preço\n";
+menuLanches += "1 -> X-SALADA-> R$ 20,00\n";
+menuLanches += "2 -> X-BACON-> R$ 25,00\n";
+menuLanches += "3 -> X-TUDO-> R$ 23,00\n";
+menuLanches += "4 -> BAURU -> R$ 29,99\n";
+menuLanches += "5 -> AMERICANO -> 22,50\n";
+menuLanches += "\n Digite o código do lanche que deseja pedir:";
+var códigoLanche = parseInt(prompt(menuLanches));
+ var quantidadeLanche = parseInt(prompt("Quantas laches deseja pedir:"));
+var valorTotal = 0;
+if (codigoLanche = 1){
+    
+}
