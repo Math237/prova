a70ffb346346bdbdcbdaff140cd4f207a7eecb14
Math237 <richardgarcia28688@gmail.com>
2024-09-26 21:24:07 -0300

    prova

--- ex3/ex3.js
+++ ex3/ex3.js
@@ -6,14 +6,15 @@
 var nota1 = parseFloat(prompt("Digite a nota do 1 Bimestre "));
 var nota2 = parseFloat(prompt("Digite a nota do 2 Bimestre "));
 var nota3 = parseFloat(prompt("Digite a nota do 3 Bimestre "));
-var nota4 = parseFloat(picou de recuperação");
-}else if(media < 5){rompt("Digite a nota do 4 Bimestre "));
+var nota4 = parseFloat(prompt("Digite a nota do 4 Bimestre "));
 var media = (nota1 + nota2 + nota3 + nota4) / 4;
 if (media >= 7){
     console.log("A sua média é de: " + media + "você foi aprovado");
 }else if(media >= 5  && media < 7){
-    console.log("A sua média é de: " + media + "você f
+    console.log("A sua média é de: " + media + "você ficou de recuperação");
+}else if(media < 5){
     console.log("A sua média é de: " + media + "você foi reprovado");
-}else {
     console.log("Digite uma nota válida");
+}else{
+    console.log("A sua média é de: " + media + "você foi reprovado");
 }
